ab50c03cea81e1b23e4b5a480f27253a6ed11f68
Felipe Cesar <felipecesr@gmail.com>
2025-03-26 19:43:33 -0300

    feat: webhook route

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.post('/', (req, res) => {
-  res.send('Got a POST request')
-})
+app.post("/", (req, res) => {
+  res.send("Got a POST request");
+});
+
+app.get("/webhooks", (req, res) => {
+  if (
+    req.query["hub.mode"] == "subscribe" &&
+    req.query["hub.verify_token"] == token
+  ) {
+    res.send(req.query["hub.challenge"]);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+app.post("/webhooks", (req, res) => {
+  const body = JSON.parse(req.body);
+  if (body.field !== "messages") {
+    // not from the messages webhook so dont process
+    return res.sendStatus(400);
+  }
+  res.send(JSON.stringify(body));
+});
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Server is running...')
-})
+  console.log("Server is running...");
+});
