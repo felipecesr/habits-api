f38069825fad4cfc7310a80b4076c65c3f42d345
Felipe César <felipecesr@gmail.com>
2025-06-21 12:04:01 -0300

    feat: contact operations

--- src/routes/contacts.ts
+++ src/routes/contacts.ts
@@ -1,17 +1,35 @@
 import { Router } from "express";
+import { body } from "express-validator";
+import { handleInputErrors } from "../modules/middleware";
+import {
+  createContact,
+  deleteContact,
+  getContacts,
+  getOneContact,
+  updateContact,
+} from "../handlers/contacts";
 
 const contactsRouter = Router({ mergeParams: true });
 
-contactsRouter.get("/", (req, res) => {
-  res.json({ message: "contacts" });
-});
+contactsRouter.get("/", getContacts);
 
-contactsRouter.post("/", (req, res) => {});
+contactsRouter.post(
+  "/",
+  body("name").isString(),
+  body("whatsappNumber").isNumeric(),
+  handleInputErrors,
+  createContact
+);
 
-contactsRouter.get("/:contactsId", (req, res) => {});
+contactsRouter.get("/:contactsId", getOneContact);
 
-contactsRouter.put("/:contactsId", (req, res) => {});
+contactsRouter.put(
+  "/:contactsId",
+  body("name").optional(),
+  body("whatsappNumber").optional(),
+  updateContact
+);
 
-contactsRouter.delete("/:contactsId", (req, res) => {});
+contactsRouter.delete("/:contactsId", deleteContact);
 
 export default contactsRouter;
